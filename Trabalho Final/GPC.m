9792dba90e0e838e9b6cd7a90f4408502eecfee8
Ricardo Machado <72943956+ricardoamachado@users.noreply.github.com>
2026-05-26 20:51:48 -0300

    Determinação das matrizes G, F e H do GPC

--- Trabalho Final/GPC.m
+++ Trabalho Final/GPC.m
@@ -2,14 +2,14 @@ clear;
 
 load("sys_disc.mat")
 [B_poly, A_poly] = ss2tf(sys_disc.A,sys_disc.B,sys_disc.C,sys_disc.D);
-
 sys_tf = tf(B_poly,A_poly,sys_disc.Ts,'Variable', 'z^-1');
 N_predicao = 10;
 N_controle = 8;
 
 % Função de transf. na forma (z^-1)*(B(z^-1))/(A(z^-1))
 B_poly = B_poly(2:end); % Retira o z^-1.
-% Multiplica A por (1-z^-1)
+
+% Multiplica A por (1-z^-1) para obter o A~.
 A_til = conv(A_poly, [1, -1]);
 
 %Cada linha j de E representa o polinômio E_j.
@@ -20,11 +20,15 @@ A_til = conv(A_poly, [1, -1]);
 tamanho_E = size(E, 2);
 tamanho_B = length(B_poly);
 tamanho_conv = tamanho_E + tamanho_B - 1;
-num_linhas = size(E, 1);
-M = zeros(num_linhas, tamanho_conv);
-%Calcula o produto M_j = E_j * B_j e armazena em M
-for idx = 1:num_linhas
+
+M = zeros(N_predicao, tamanho_conv);
+% Matriz H multiplica os incrementos de controle passados.
+% N linhas e n_b colunas, com n_b grau do polinômio B(z^-1).
+H = zeros(N_predicao,tamanho_B-1);
+%Calcula o produto M_j = E_j * B_j e armazena em M.
+for idx = 1:N_predicao
     M(idx, :) = conv(E(idx, :), B_poly);
+    H(idx,:) = M(idx, idx+1:idx+tamanho_B-1);
 end
 
 % Separa o vetor com coeficientes da resp. ao degrau.
